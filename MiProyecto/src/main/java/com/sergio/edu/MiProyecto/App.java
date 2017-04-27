820d4051177b6f6510907df030e51a2f57945599
feliperh <feliperh96@gmail.com>
2017-04-27 18:09:47 -0500

    Add files via upload

---
+++ MiProyecto/src/main/java/com/sergio/edu/MiProyecto/App.java
@@ -0,0 +1,13 @@
+package com.sergio.edu.MiProyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
